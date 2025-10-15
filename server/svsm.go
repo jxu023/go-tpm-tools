f8a533f520573632692a38a25894c7201a3fffb5
Jacob Xu <jacobhxu@google.com>
2025-10-15 18:30:59 +0000

    implement EK-based Key attestation for the SVSM e-vTPM
    The client server files for import_certify.go corresponds to the spec
    for EK-based Key attestation with firmware version.
    See https://trustedcomputinggroup.org/wp-content/uploads/EK-Based-Key-Attestation-with-TPM-Firmware-Version-V1-RC1_9July2025.pdf#page=8
    
    This is to be used with Coconut SVSM's single service attestation for
    the vTPM. We use SVSM's service attestation and the SNP hardware
    attestation report to create trust for the EK, instead of an EK
    certificate.
    See https://github.com/coconut-svsm/svsm

---
+++ server/svsm.go
@@ -0,0 +1,48 @@
+package server
+
+import (
+	"bytes"
+	"crypto/sha512"
+	"fmt"
+
+	"github.com/google/go-sev-guest/verify"
+	rpb "github.com/google/go-tpm-tools/proto/register_credential"
+)
+
+type VerifySVSMOpts struct {
+	TEENonce    []byte
+	Attestation *rpb.SevSnpSvsmAttestation
+	Secret      []byte
+}
+
+// VerifySVSMAttestation checks the SNP attestation report to ensure it was requested by
+// SVSM from VMPL0 and that report data contains a hash of the tee nonce and vTPM service manifest.
+// Other SNP attestation report values (including the launch measurement)
+// should be checked via gce tcb verifier's sev validate command.
+func VerifySVSMAttestation(opts *VerifySVSMOpts) error {
+	err := verifyChallenge(opts.Attestation.GetCertifiedBlob(), opts.Secret)
+	if err != nil {
+		return fmt.Errorf("challenge verification failed: %w", err)
+	}
+
+	h := sha512.New()
+	h.Write(opts.TEENonce[:])
+	// Service manifest is the Ekpub.
+	h.Write(opts.Attestation.GetVtpmServiceManifest())
+	expectedReportData := h.Sum(nil)
+	report := opts.Attestation.GetSevSnpAttestation().GetReport()
+
+	if !bytes.Equal(report.GetReportData(), expectedReportData) {
+		return fmt.Errorf("report data does not match expected value")
+	}
+	if report.Vmpl != 0 {
+		return fmt.Errorf("attestation report was not request from VMPL0, SVSM e-vTPM should be in VMPL0")
+	}
+	// Check the signature, certificate chain, and basic
+	// well-formedness properties of the SNP attestation report.
+	err = verify.SnpAttestation(opts.Attestation.GetSevSnpAttestation(), &verify.Options{})
+	if err != nil {
+		return fmt.Errorf("SNP attestation verification failed: %w", err)
+	}
+	return nil
+}
